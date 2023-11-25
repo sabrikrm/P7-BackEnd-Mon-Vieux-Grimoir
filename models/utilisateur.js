451c467d5e8bef0a4b6393ea9d358ce567e86055
sabri karoumi <skaroumi71@gmail.com>
2023-11-25 11:35:27 +0100

    avant soutenance

--- models/utilisateur.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const utilisateurSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true,  },
-  
-});
-
-module.exports = mongoose.model('Utilisateur', utilisateurSchema);
